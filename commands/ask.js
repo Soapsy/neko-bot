a0b7f5ec100394c1237400e0997142e0a8879864
Glitch (sudnobot) <none>
2018-12-20 16:56:19 +0000

    🐋😥 Checkpoint ./commands/ask.js:2501470/408

--- commands/ask.js
+++ commands/ask.js
@@ -1,10 +1,16 @@
 exports.run = (client, message, args) => {
   if(args[0] && !message.author.bot)
   {
+    
+    const emoji = message.guild.emojis.random();
 	const randcha = require('../data/answers.json');
 	const leng = randcha.length + 1;
 	const rnd = Math.floor(Math.random() * (leng - 0));
-	const reply = randcha[rnd];
+    const rnd2 = Math.floor(Math.random() * (3 - 0));
+    let reply = randcha[rnd];
+    if(rnd2 == 0){
+      reply += " " + emoji;
+    }
     message.channel.send(reply).catch(console.error);
   }
 }
