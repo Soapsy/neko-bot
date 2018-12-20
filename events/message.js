cf5b558457f484f584852518ec4a9d449292e527
Glitch (sudnobot) <none>
2018-12-20 17:07:28 +0000

    👰💍 Checkpoint ./data/reacts.json:2501470/95 ./events/message.js:2501470/82

--- events/message.js
+++ events/message.js
@@ -11,7 +11,7 @@ const chan = message.channel;
         const message_text = message.content;
         console.log(message_text);
         const rnd = Math.round(Math.floor(Math.random() * (11- 1)) + 1);
-        if (rnd === 10|| 2 || 6 || 8 || 4) {
+        if ((rnd === 10) || (rnd === 4) || (rnd === 6) || (rnd === 2)) {
             if ((message.content.toLowerCase().includes("баклажан")) || (message.content.toLowerCase().includes("eggplant"))) {
                 if (message.author.bot) return; //	защита от лупов
                 message.react("🍆").catch(console.error);
