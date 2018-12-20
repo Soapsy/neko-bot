e6f469a6f417e083285c4401d347e07ee2bc62ae
Glitch (sudnobot) <none>
2018-12-20 16:23:20 +0000

    🐃📙 Checkpoint ./commands/8ball.js:2501470/2 ./answers.json:2501470/696 ./commands/ask.js:2501470/378 ./commands/choose.js:2501470/60 ./commands/randchar.js:2501470/28 ./data/answers.json:2501470/39

--- commands/ask.js
+++ commands/ask.js
@@ -1,6 +1,5 @@
 exports.run = (client, message, args) => {
-	const randcha = require('../data/chars.json');
-	console.log(randcha);
+	const randcha = require('../data/answers.json');
 	const leng = randcha.length + 1;
 	const rnd = Math.floor(Math.random() * (leng - 0));
 	const reply = randcha[rnd];
