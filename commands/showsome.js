ff111dc3a718b5d86f0839be3344b12ab9e62b6f
Glitch (sudnobot) <none>
2019-06-19 21:46:13 +0000

    🌘🍙 Checkpoint ./commands/showsome.js:2501470/508

--- commands/showsome.js
+++ commands/showsome.js
@@ -1,22 +1,12 @@
 exports.run = (client, message, args) => {
-  if(message.guild.id != process.env.HOMESERVER)return;
-  if(args[0] && !message.author.bot)
+  //if(message.guild.id != process.env.HOMESERVER)return;
+  if(!message.author.bot)
   {
-    
-    
-     fs.readFile('data/ping.txt', function(err, data){
+    const fs = require('fs');
+    fs.readFile('../data/ping.txt', function(err, data){
     if(err) throw err;
     var lines = data.split('\n');
-    vlines[Math.floor(Math.random()*lines.length)];
-       
-	const randcha = require('../data/answers.json');
-	const leng = randcha.length + 1;
-	const rnd = Math.floor(Math.random() * (leng - 0));
-    const rnd2 = Math.floor(Math.random() * (3 - 0));
-    let reply = randcha[rnd];
-    if(rnd2 == 0 && emoji){
-      reply += " " + emoji;
-    }
-    message.channel.send(reply).catch(console.error);
-  }
+    var link = lines[Math.floor(Math.random()*lines.length)];
+    message.channel.send(link).catch(console.error);
+  })}
 }
